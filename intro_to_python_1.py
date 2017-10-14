904e3017b0c31d8f0d801bdc950ad7306bb85522
Tony <tonyliou4@gmail.com>
2017-10-13 21:36:18 -0400

    Added a tutorial file.

---
+++ intro_to_python_1.py
@@ -0,0 +1,26 @@
+print "Hello world!"
+print "This is a demo program!"
+# Print tells the computer what to output in the command prompt.
+# If you run the program, this will apear.
+
+x = 5  #This sets the variable 'x' to 5.
+# The numer '5' is an int. Python is nice that you don't have to set the variable 'x' as an int.
+# In java, you would have to say int x = 5. In python, it's just x = 5.
+
+y = "variable" # This sets the variable 'y' to the world 'variable'.
+
+print y # This will print what ever is stored in y. As of now, it is 'variable'
+
+if x == 5: # If the variable, 'x', is 5, this is done. MUST BE ==. == is compareing, = is setting. x = 5 will return an error.
+	print "The varible, x, is 5" # 
+# A ta means it is currently done in the if.
+
+if x == 7:
+	print "The Varible, x, is 7."
+# This wont happen, as 'x' is not 7.
+
+if x > 6:
+	print "The Variable, x, is less than 6."
+	# There are multiple comparison things. >= (Greater than or equals to) <= (Less than or equal to) <, >, ==, and != (Not equals to)
+
+	
