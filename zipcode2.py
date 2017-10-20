76aaeae3983497e115c3aab07b2bf60c3c02ba10
Daniel Zhao <danielzhao2@gmail.com>
2017-10-20 16:39:46 -0400

    Some experimental stuff. Ignore for now

--- zipcode2.py
+++ zipcode2.py
@@ -3,63 +3,47 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
+import json
 wb = Workbook()
-#https://openpyxl.readthedocs.io/en/default/
+def DumpDict(ZipCode):
+	with open(zipcode.json, 'w') as f:
+		json.dump(ZipCode, f)	
+def FindZipcode(city, state):
+	zipcode = "Null"
+	url = "http://www.city-data.com/zipmaps/"+city+"-"+state+".html"
+	r = requests.get(url)
+	soup = BeautifulSoup(r.content, "html.parser")
 
-file = open("plaintexttxt.txt","w")
+	zipcode = soup.find_all("div",{"class": "zip-codes"}) [0].text
+	zipcode = str(zipcode)
 
+	print zipcode
+	zipcode = zipcode.replace("Z","")
+	zipcode = zipcode.replace("i","")
+	zipcode = zipcode.replace("p","")
+	zipcode = zipcode.replace("c","")
+	zipcode = zipcode.replace("o","")
+	zipcode = zipcode.replace("d","")
+	zipcode = zipcode.replace("e","")
+	zipcode = zipcode.replace("s","")
+	zipcode = zipcode.replace(":","")						
+	zipcode = zipcode.replace(",","|")
+	zipcode = zipcode.replace(".","")
+	zipcode = zipcode.replace(" ","")
+	zipcode = zipcode.split("|")
+	StringZipcode = ''.join(zipcode)
+	x = 0
+	ZipCode = dict()
+	ZipCode['0'] = {'City': city,  	'State': state, 'ZipCodes': StringZipcode}
+	DumpDict(ZipCode)
+FindZipcode('Akron',"Ohio")
 
-print "Captitalise the first letter of each word. Replace spaces with \'-\'"
-print "Valid:San-Francisco"
-print "Valid:California"
-print "Invalid san francisco"
-print "Washington DC is city:Washington state:District-of-Columbia"
-zipcode = "Null"
-while True:
-	try:
-		city = raw_input("City>")
-		state = raw_input("State>")	
-	
-		url = "http://www.city-data.com/zipmaps/"+city+"-"+state+".html"
-		r = requests.get(url)
-		soup = BeautifulSoup(r.content, "html.parser")
-	
-		zipcode = soup.find_all("div",{"class": "zip-codes"}) [0].text
-		zipcode = str(zipcode)
-	
-		print zipcode
-		zipcode = zipcode.replace("Z","")
-		zipcode = zipcode.replace("i","")
-		zipcode = zipcode.replace("p","")
-		zipcode = zipcode.replace("c","")
-		zipcode = zipcode.replace("o","")
-		zipcode = zipcode.replace("d","")
-		zipcode = zipcode.replace("e","")
-		zipcode = zipcode.replace("s","")
-		zipcode = zipcode.replace(":","")						
-		zipcode = zipcode.replace(",","|")
-		zipcode = zipcode.replace(".","")
-		zipcode = zipcode.replace(" ","")
-		zipcode = zipcode.split("|")
-		length = len(zipcode)
-		x = 0
-		while True:
-			print zipcode[x]
-			
-			if x == length - 1:
-				break
-			x+=1
-	
-		file.write(city +"\n"+ state)
-	
-		for entry in zipcode:
-		  file.write(entry)
-		break
-	
-	except:
-		print "Invalid City and/or State"
+#def Start():
+#	print "Would you like to add to the dict or read the dict?"
+#
+
+#def ReadDict():
+#
 
 
-
-print "Done"
 #<div class="discount_final_price">$29.99</div>
